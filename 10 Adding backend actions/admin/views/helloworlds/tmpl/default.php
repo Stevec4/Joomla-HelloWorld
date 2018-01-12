63f1683001a5336433afa7bfebe4ca7d0f3303f1
Steve C <6751354+Stevec4@users.noreply.github.com>
2018-01-12 17:23:11 -0500

    Add files via upload

---
+++ 10 Adding backend actions/admin/views/helloworlds/tmpl/default.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted Access');
+?>
+<form action="index.php?option=com_helloworld&view=helloworlds" method="post" id="adminForm" name="adminForm">
+	<table class="table table-striped table-hover">
+		<thead>
+		<tr>
+			<th width="1%"><?php echo JText::_('COM_HELLOWORLD_NUM'); ?></th>
+			<th width="2%">
+				<?php echo JHtml::_('grid.checkall'); ?>
+			</th>
+			<th width="90%">
+				<?php echo JText::_('COM_HELLOWORLD_HELLOWORLDS_NAME') ;?>
+			</th>
+			<th width="5%">
+				<?php echo JText::_('COM_HELLOWORLD_PUBLISHED'); ?>
+			</th>
+			<th width="2%">
+				<?php echo JText::_('COM_HELLOWORLD_ID'); ?>
+			</th>
+		</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<td colspan="5">
+					<?php echo $this->pagination->getListFooter(); ?>
+				</td>
+			</tr>
+		</tfoot>
+		<tbody>
+			<?php if (!empty($this->items)) : ?>
+				<?php foreach ($this->items as $i => $row) :
+					$link = JRoute::_('index.php?option=com_helloworld&task=helloworld.edit&id=' . $row->id);
+				?>
+					<tr>
+						<td><?php echo $this->pagination->getRowOffset($i); ?></td>
+						<td>
+							<?php echo JHtml::_('grid.id', $i, $row->id); ?>
+						</td>
+						<td>
+							<a href="<?php echo $link; ?>" title="<?php echo JText::_('COM_HELLOWORLD_EDIT_HELLOWORLD'); ?>">
+								<?php echo $row->greeting; ?>
+							</a>
+						</td>
+						<td align="center">
+							<?php echo JHtml::_('jgrid.published', $row->published, $i, 'helloworlds.', true, 'cb'); ?>
+						</td>
+						<td align="center">
+							<?php echo $row->id; ?>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+			<?php endif; ?>
+		</tbody>
+	</table>
+	<input type="hidden" name="task" value=""/>
+	<input type="hidden" name="boxchecked" value="0"/>
+	<?php echo JHtml::_('form.token'); ?>
+</form>
---
+++ 10 Adding backend actions/admin/views/helloworlds/view.html.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * HelloWorlds View
+ *
+ * @since  0.0.1
+ */
+class HelloWorldViewHelloWorlds extends JViewLegacy
+{
+	/**
+	 * Display the Hello World view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
+	 */
+	function display($tpl = null)
+	{
+		// Get data from the model
+		$this->items		= $this->get('Items');
+		$this->pagination	= $this->get('Pagination');
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors')))
+		{
+			JError::raiseError(500, implode('<br />', $errors));
+
+			return false;
+		}
+
+		// Set the toolbar
+		$this->addToolBar();
+
+		// Display the template
+		parent::display($tpl);
+	}
+
+	/**
+	 * Add the page title and toolbar.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.6
+	 */
+	protected function addToolBar()
+	{
+		JToolbarHelper::title(JText::_('COM_HELLOWORLD_MANAGER_HELLOWORLDS'));
+		JToolbarHelper::addNew('helloworld.add');
+		JToolbarHelper::editList('helloworld.edit');
+		JToolbarHelper::deleteList('', 'helloworlds.delete');
+	}
+}
