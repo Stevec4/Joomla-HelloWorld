5e3be869fdc41917b4b9110896545c2e4dac771e
Steve C <6751354+Stevec4@users.noreply.github.com>
2018-01-12 17:11:01 -0500

    Add files via upload

---
+++ 05 Adding a model to the site part/site/models/helloworld.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * HelloWorld Model
+ *
+ * @since  0.0.1
+ */
+class HelloWorldModelHelloWorld extends JModelItem
+{
+	/**
+	 * @var string message
+	 */
+	protected $message;
+
+	/**
+	 * Get the message
+         *
+	 * @return  string  The message to be displayed to the user
+	 */
+	public function getMsg()
+	{
+		if (!isset($this->message))
+		{
+			$this->message = 'Hello World!';
+		}
+
+		return $this->message;
+	}
+}
