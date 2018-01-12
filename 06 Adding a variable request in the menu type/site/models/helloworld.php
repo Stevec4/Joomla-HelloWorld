7d8e32a35c28347680a5ab2295cf6b7770e74e43
Steve C <6751354+Stevec4@users.noreply.github.com>
2018-01-12 17:15:47 -0500

    Add files via upload

---
+++ 06 Adding a variable request in the menu type/site/models/helloworld.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * HelloWorld Model
+ *
+ * @since  0.0.1
+ */
+class HelloWorldModelHelloWorld extends JModelItem
+{
+	/**
+	 * @var string message
+	 */
+	protected $message;
+
+	/**
+	 * Get the message
+         *
+	 * @return  string  The message to be displayed to the user
+	 */
+	public function getMsg()
+	{
+		if (!isset($this->message))
+		{
+			$jinput = JFactory::getApplication()->input;
+			$id     = $jinput->get('id', 1, 'INT');
+
+			switch ($id)
+			{
+				case 2:
+					$this->message = 'Good Bye World!';
+					break;
+				default:
+				case 1:
+					$this->message = 'Hello World!';
+					break;
+			}
+		}
+		return $this->message;
+	}
+}
