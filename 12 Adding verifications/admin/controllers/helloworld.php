19fd61d3180909eb4f9752f1c1eb70088831f645
Steve C <6751354+Stevec4@users.noreply.github.com>
2018-01-12 20:52:11 -0500

    Add files via upload

---
+++ 12 Adding verifications/admin/controllers/helloworld.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * HelloWorld Controller
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ * @since       0.0.9
+ */
+class HelloWorldControllerHelloWorld extends JControllerForm
+{
+}
