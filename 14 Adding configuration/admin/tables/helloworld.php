97649e2f89f441bdc428b45925605f640dac4946
Steve C <6751354+Stevec4@users.noreply.github.com>
2018-01-13 06:31:26 -0500

    Add files via upload

---
+++ 14 Adding configuration/admin/tables/helloworld.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Hello Table class
+ *
+ * @since  0.0.1
+ */
+class HelloWorldTableHelloWorld extends JTable
+{
+	/**
+	 * Constructor
+	 *
+	 * @param   JDatabaseDriver  &$db  A database connector object
+	 */
+	function __construct(&$db)
+	{
+		parent::__construct('#__helloworld', 'id', $db);
+	}
+	/**
+	 * Overloaded bind function
+	 *
+	 * @param       array           named array
+	 * @return      null|string     null is operation was satisfactory, otherwise returns an error
+	 * @see JTable:bind
+	 * @since 1.5
+	 */
+	public function bind($array, $ignore = '')
+	{
+		if (isset($array['params']) && is_array($array['params']))
+		{
+			// Convert the params field to a string.
+			$parameter = new JRegistry;
+			$parameter->loadArray($array['params']);
+			$array['params'] = (string)$parameter;
+		}
+		return parent::bind($array, $ignore);
+	}
+}
