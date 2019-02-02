c799a1fd260e3c5488582c89f4d505472e7e67f0
Steve C <6751354+Stevec4@users.noreply.github.com>
2019-02-02 08:32:47 -0500

    Add files via upload

--- 04 Adding a menu type to the site part/site/controller.php
+++ 04 Adding a menu type to the site part/site/controller.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Administrator
  * @subpackage  com_helloworld
  *
- * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 // No direct access to this file
@@ -13,6 +13,5 @@ defined('_JEXEC') or die('Restricted access');
  *
  * @since  0.0.1
  */
-class HelloWorldController extends JControllerLegacy
-{
+class HelloWorldController extends JControllerLegacy{
 }
