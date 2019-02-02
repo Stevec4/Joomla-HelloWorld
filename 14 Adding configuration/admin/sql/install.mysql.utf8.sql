949f7c93c8fa09ce370e5ee9e0472710c41c0015
Steve C <6751354+Stevec4@users.noreply.github.com>
2019-02-02 08:58:11 -0500

    Add files via upload

--- 14 Adding configuration/admin/sql/install.mysql.utf8.sql
+++ 14 Adding configuration/admin/sql/install.mysql.utf8.sql
@@ -3,16 +3,17 @@ DROP TABLE IF EXISTS `#__helloworld`;
 CREATE TABLE `#__helloworld` (
 	`id`       INT(11)     NOT NULL AUTO_INCREMENT,
 	`greeting` VARCHAR(25) NOT NULL,
-	`published` tinyint(4) NOT NULL,
+	`published` tinyint(4) NOT NULL DEFAULT '1',
 	`catid`	    int(11)    NOT NULL DEFAULT '0',
 	`params`   VARCHAR(1024) NOT NULL DEFAULT '',
 	PRIMARY KEY (`id`)
 )
-
-	ENGINE =MyISAM
+	ENGINE=InnoDB 
 	AUTO_INCREMENT =0
-	DEFAULT CHARSET =utf8;
-
+	DEFAULT CHARSET=utf8mb4 
+	DEFAULT COLLATE=utf8mb4_unicode_ci;
+	
+	
 INSERT INTO `#__helloworld` (`greeting`) VALUES
 ('Hello World!'),
 ('Good Bye World!');
